3a48098acf9af78cd52bd216335c42fb3a08a687
yohanmanohara <warushayohan80@gmail.com>
2024-12-02 11:15:56 +0530

    #

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -129,14 +129,14 @@ const Contact = () => {
   const form = useRef();
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    emailjs.sendForm('service_tox7kqs', 'template_nv7k7mj', form.current, 'SybVGsYS52j2TfLbi')
-      .then((result) => {
-        setOpen(true);
-        form.current.reset();
-      }, (error) => {
-        console.log(error.text);
-      });
+    // e.preventDefault();
+    // emailjs.sendForm('service_tox7kqs', 'template_nv7k7mj', form.current, 'SybVGsYS52j2TfLbi')
+    //   .then((result) => {
+    //     setOpen(true);
+    //     form.current.reset();
+    //   }, (error) => {
+    //     console.log(error.text);
+    //   });
   }
 
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -14,7 +14,7 @@ const Navbar = () => {
       <NavbarContainer>
         <NavLogo to='/'>
           <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20;', cursor: 'pointer' }}>
-            <DiCssdeck size="3rem" /> <Span>YOHAN MANOHARA</Span>
+            <Span>YOHAN MANOHARA</Span>
           </a>
         </NavLogo>
         <MobileIcon>
