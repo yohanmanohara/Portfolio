7b3ba7f6ce6564675519df051aa660542dde43bd
yohanmanohara <warushayohan80@gmail.com>
2024-12-03 07:56:41 +0530

    #

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { inject } from "@vercel/analytics";
 import App from './App';
+inject()
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
